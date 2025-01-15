7234e3d2bbfea10da3a442b33f9a9b4db077ce94
StevenIsHereIGuess <stevenishereiguess@gmail.com>
2025-01-15 18:03:51 -0500

    I should have finished 1 other requirment, I think, I hope, I know

---
+++ 2d_game_mechanics_2/CheckpointScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerLife playerLife = other.GetComponent<PlayerLife>();
+            if (playerLife != null)
+            {
+                playerLife.SetCheckpoint(transform.position);
+            }
+        }
+    }
+}
